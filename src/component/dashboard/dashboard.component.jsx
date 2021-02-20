ba7bc67955aaae1d2d16530340e0d3d7038dc5da
Marian Zburlea <marianzburlea@gmail.com>
2021-02-20 12:49:25 +0000

    super commit, zice Bogdan

--- src/component/dashboard/dashboard.component.jsx
+++ src/component/dashboard/dashboard.component.jsx
@@ -1,19 +1,50 @@
 import { Button } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { db } from '../../config/firebase'
 
 export const Dashboard = () => {
+  const [list, setList] = useState([])
+  // vreau sa chem ceva o singura data
+  useEffect(() => {
+    // si asa facem ceva o singura data
+    db.collection('felix-si-brando').onSnapshot(snapshot => {
+      setList(snapshot.docs.map(test => ({
+        id: test.id,
+        ...test.data()
+      })))
+      // console.log(snapshot.size)
+    })
+  }, [])
+
   const adaugaAcumaSauTeFacPunctePunctePuncte = () => {
     // vrem sa adaugam ceva intr-o baza de date smecheroasa
-    console.log('yeee, a venit razatoarea')
     db.collection('felix-si-brando').add({
       name: 'Super eroii',
       age: 912234,
       qualities: [
         'nice',
-        'smart'
-      ]
+        'smart',
+        'funny',
+        'razatoare',
+        'unicorn trainer',
+        'smiley',
+        'brando',
+        'happy',
+        'cool',
+        'bright',
+        'water',
+        'catel',
+        'latrat',
+        'ham-ham',
+        'miau miau',
+        'bunny',
+      ].sort(() => Math.random() - 0.5).slice(0, Math.round(Math.random() * 7))
     })
+  }
+
+  const deleteItem = id => {
+    console.log(`Sterge-l pe ala cu id: ${id}`)
+    db.collection('felix-si-brando').doc(id).delete()
   }
 
   return (
@@ -26,6 +57,25 @@ export const Dashboard = () => {
       >
         Add something to the db
       </Button>
+
+      <div>
+        <ul>
+          {list.map(({ age, name, qualities, id }) => {
+            return (
+              <li>
+                <div>{age}</div>
+                <div>{name}</div>
+                <div>
+                  <div>
+                    {qualities.map(quality => <span style={{color: 'green'}}>{quality} </span> )}
+                  </div>
+                  <Button onClick={() => deleteItem(id)}>&times;</Button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
